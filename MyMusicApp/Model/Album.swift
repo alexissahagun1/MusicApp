67096cfc08bdfc9124ef078afc48151ba6db49e4
Manuel Cota <56351434+nihonboi@users.noreply.github.com>
2021-10-06 11:02:40 -0500

    First commit

---
+++ MyMusicApp/Model/Album.swift
@@ -0,0 +1,17 @@
+//
+//  Album.swift
+//  MyMusicApp
+//
+//  Created by gdaalumno on 06/10/21.
+//
+
+import SwiftUI
+
+struct Album {
+    var title: String = ""
+    var artist: String = ""
+    var type: String = ""
+    var artwork: UIImage = UIImage(named: "music")!
+    
+}
+
---
+++ MyMusicApp/MyMusicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMusicAppApp.swift
+//  MyMusicApp
+//
+//  Created by gdaalumno on 06/10/21.
+//
+
+import SwiftUI
+
+@main
+struct MyMusicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
